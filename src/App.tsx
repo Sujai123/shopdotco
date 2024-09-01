27aafeb1a87ccca931dfd7ce8fe566b7f41fa2b8
Sujai123 <sujaibeniks001@gmail.com>
2024-09-01 17:10:48 +0530

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import theme from './constants/theme'
+import Home from './pages/Home';
+import BaseLayout from './layout/BaseLayout';
 
 function App() {
 
   return (
-    <div>
-      test
-    </div>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<BaseLayout />}>
+            <Route path='/' element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
